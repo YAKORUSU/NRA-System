bfaf8d4250fcfa44abe070319a10f5c16c415d6c
YAKORUSU <95697718+YAKORUSU@users.noreply.github.com>
2023-07-03 16:16:22 +0900

    読み上げシステム構築

--- app/result_notification.py
+++ app/result_notification.py
@@ -62,7 +62,36 @@ def result_info(date:str):
     except:
         return "error"
     return race_list
+    
+@app.get("/synthesize/{textrequest}")
+async def synthesize_text(textrequest:str):
+    
+    # リクエストのデータをTextRequestオブジェクトに変換
+    text_request = urllib.parse.unquote(f"{textrequest}")
+    # return text_request
+    print(text_request)
+    # Azure Cognitive Services Speech SDKの設定
+    speech_key = "YourSubscriptionKey"
+    service_region = "YourServiceRegion"
+    audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+    
+    # テキストをwavに変換
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+    speech_config.speech_synthesis_voice_name='ja-JP-AoiNeural'
+    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+    result = speech_synthesizer.speak_text_async(text_request).get()
 
+# 音声を取得
+    audio_data = result.audio_data
+
+    # 音声を一時ファイルに保存
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
+        temp_filename = temp_file.name
+        temp_file.write(audio_data)
+    
+    # wavファイルをリターン
+    return FileResponse(temp_filename, media_type="audio/wav")
+    
 @app.websocket("/ws/result")
 async def websocket_endpoint(ws: WebSocket):
     await ws.accept()
