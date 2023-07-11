1728fdf85b8fdffddcca5b24b28a7e404869a296
YAKORUSU <95697718+YAKORUSU@users.noreply.github.com>
2023-07-11 21:17:41 +0900

    Chromeの動作軽量化実装

--- app/baken_list.py
+++ app/baken_list.py
@@ -1,25 +1,26 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from rich import print,pretty
+from rich import print, pretty
 from rich.console import Console
 
 console = Console()
 pretty.install()
-#chomedriverのパスを指定
-executable_path="./chromedriver.exe"
+# chromedriverのパスを指定
+executable_path = "./chromedriver.exe"
 
-result_raceID_list = []
-done_raceID_list = []
-result_raceID_dict = {}
 options = Options()
-options.add_argument('--headless')
+options.add_argument('--headless')  # ヘッドレスモードで起動
+options.add_argument('--disable-gpu')  # GPUの無効化
+options.add_argument('--no-sandbox')  # サンドボックスモードの無効化
+options.add_argument('--disable-dev-shm-usage')  # 共有メモリの無効化
 
 def horse_list(url):
-    # HTMLを取得
-    driver = webdriver.Chrome(executable_path=executable_path, options=options)
-    driver.get(url)
-    html = driver.page_source
+    # WebDriverの初期化
+    with webdriver.Chrome(executable_path=executable_path, options=options) as driver:
+        # HTMLを取得
+        driver.get(url)
+        html = driver.page_source
 
     # BeautifulSoupを使用してHTMLを解析
     soup = BeautifulSoup(html, 'html.parser', from_encoding='UTF-8')
@@ -29,24 +30,27 @@ def horse_list(url):
 
     # 表のデータを抽出してJson形式に整形
     table = soup.find('tbody')
-    rows = table.find_all('tr', class_='HorseList') # ヘッダー行を除外
-    result = {}
-    result['raceid'] = race_id
-    result['main'] = []
+    rows = table.find_all('tr', class_='HorseList')
+    result = {
+        'raceid': race_id,
+        'main': {}
+    }
     for row in rows:
         columns = row.find_all('td')
         number = columns[1].text.strip()
         horse_name = columns[3].text.strip()
         frame = int(columns[0].text.strip())
         odds = float(columns[9].text.strip())
-      　ninki = int(columns[10].text.sprip())
+        ninki = int(columns[10].text.strip())
 
         result['main'][number] = {
             '馬名': horse_name,
             '枠': frame,
             'オッズ': odds,
-            '人気': ninki          
+            '人気': ninki
         }
 
-    response_json = f"{result}"
+    response_json = f'{result}'
+
     return response_json
+
